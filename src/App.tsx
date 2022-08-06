0d7467456bf15c74212e61d40f4a84e7835bfd9f
Lawson Fulton <lawsonfulton@gmail.com>
2022-08-06 15:03:28 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,73 @@
 import { Canvas } from "@react-three/fiber";
 
-import React from "react";
+import React, { useState } from "react";
 import Viewer3D from "./Viewer3D";
 import SketchArea from "./SketchArea";
 import ToolBar from "./ToolBar";
 import 'antd/dist/antd.css';
 
+export class AppState {
+  points: number[] = [];
+  linePoints: number[] = [];
+  isDrawing: boolean = false;
+}
+export interface StateCallbacks {
+  addPoint: (x: number, y: number) => void;
+  setPoint: (i: number, x: number, y: number) => void;
+  setLinePoint: (i: number, x: number, y: number) => void;
+}
+
 const App = () => {
+  const [state, setState] = useState<AppState>(new AppState());
+
+  const stateCallbacks: StateCallbacks = {
+    addPoint: (x: number, y: number) => {
+      setState({ ...state, points: [...state.points, x, y], linePoints: [...state.linePoints, x, y] })
+      // setState((prevState) => {
+      //   const newPoints = [...prevState.points, x, y];
+      //   return { ...prevState, points: newPoints };
+      // })
+    },
+
+    setLinePoint: (i: number, x: number, y: number) => {
+      const newPoints = [...state.linePoints];
+      newPoints[i] = x;
+      newPoints[i + 1] = y;
+      setState({ ...state, linePoints: newPoints });
+
+
+      // setState((prevState) => {
+      //   const newPoints = [...prevState.points];
+      //   newPoints[i] = x;
+      //   newPoints[i + 1] = y;
+      //   return { ...prevState, points: newPoints };
+      // })
+    },
+
+    setPoint: (i: number, x: number, y: number) => {
+      const newPoints = [...state.points];
+      newPoints[i] = x;
+      newPoints[i + 1] = y;
+      setState({ ...state, points: newPoints });
+
+
+      // setState((prevState) => {
+      //   const newPoints = [...prevState.points];
+      //   newPoints[i] = x;
+      //   newPoints[i + 1] = y;
+      //   return { ...prevState, points: newPoints };
+      // })
+    }
+  }
+
+
+
   return (
     <div>
       <ToolBar />
       <div style={{ display: "flex" }}>
         <div style={{ width: "50vw", height: "100vh", display: "inline-block" }}>
-          <SketchArea />
+          <SketchArea appState={state} callbacks={stateCallbacks} />
         </div>
         <div style={{ width: "50vw", height: "100vh", display: "inline-block" }}>
           <Viewer3D />
--- src/SketchArea.tsx
+++ src/SketchArea.tsx
@@ -2,66 +2,68 @@ import React, { ReactElement, useRef, useState } from "react";
 import { Stage, Layer, Rect, Text, Circle, Line } from "react-konva";
 import Konva from "konva";
 import { Points } from "@react-three/drei";
-
-class ColoredRect extends React.Component {
-  state = {
-    color: "green"
-  };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    );
-  }
-}
+import { AppState, StateCallbacks } from "./App";
 
 
-
-export default function SketchArea() {
+export default function SketchArea(props: { appState: AppState, callbacks: StateCallbacks }) {
   const [points, setPoints] = useState<number[]>([]);
+  // TODO
+  // I need to do one of two things
+  // 1. Figure out how redux works and use that? I need to figure out how to have global state
+  // without remounting the component all the time.
+  // 2. Put everything into one file ** probably do this one
+  // 2.5: do everything sketch relevant in this file, the just update global state when I'm ready to mesh? *****
+  // 3. Or just switch to doing this the procedural way.
+  const cb = props.callbacks;
+  const appState = props.appState;
+
+  // const [points, setPoints] = useState<number[]>([...appState.points]);
+  console.log("local: ", points);
+  console.log("global: ", appState.points);
 
   const onClickStage = (e) => {
-    const stage = e.currentTarget;
-    const pos = stage.getPointerPosition();
-    setPoints(points.concat([pos.x, pos.y]))
+    const stage = e.target.getStage();
+    const mousePos = stage.getPointerPosition();
+    cb.addPoint(mousePos.x, mousePos.y);
   }
+
+  const [draggingNode, setDraggingNode] = useState<number[] | undefined>(undefined);
 
   const onDragNode = (e, i) => {
-    points[i] = e.target.x();
-    points[i + 1] = e.target.y();
-    setPoints(points);
+    // cb.setPoint(i, e.target.x(), e.target.y());
+    cb.setLinePoint(i, e.target.x(), e.target.y());
   }
 
-  const Profile = (props: { points: number[] }) => {
+  const onDragEnd = (e, i) => {
+    console.log(e);
+  }
+
+  const Profile = (props: { points: number[], linePoints: number[] }) => {
     let nodes: JSX.Element[] = [];
     for (let i = 0; i < props.points.length - 1; i += 2) {
       nodes.push(
         <Circle
-          key={i}
+          key={i.toString()}
+          id={i.toString()}
           x={props.points[i]}
           y={props.points[i + 1]}
           fill={"aqua"}
-          radius={5}
+          radius={10}
           draggable={true}
-          onDragMove={(e) => onDragNode(e, i)}
+          // onMouseDown={e => setDraggingNode(i)}
+          // onMouseUp={e => setDraggingNode(-1)}
+          // onDragStart={e => setDraggingNode([i, appState.points[i], appState.points[i + 1]])}
+          // onDragEnd={e => setDraggingNode(undefined)}
+          // onDragMove={e => onDragNode(e, i)}
+          // onDragMove={(e) => onDragNode(e, i)}
+          onDragMove={e => onDragNode(e, i)}
+          onDragEnd={e => onDragEnd(e, i)}
         />
       )
     }
 
     const lines = <Line
-      points={props.points}
+      points={props.linePoints}
       stroke={"aqua"}
     />
 
@@ -75,7 +77,7 @@ export default function SketchArea() {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight} onClick={onClickStage}>
       <Layer>
-        <Profile points={points} />
+        <Profile points={appState.points} linePoints={appState.linePoints} />
       </Layer>
     </Stage>
   );
